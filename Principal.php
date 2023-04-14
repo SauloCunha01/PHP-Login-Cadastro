ebf5b0b74e2a41dd756984003015c5a2904df764
SauloCunha <saulocunha01@hotmail.com>
2023-04-14 11:18:59 -0300

    botão e script para fazer logout
    adicionei um botão de saída na página principal  e um código de verificação de autenticação do usuário, script sair.php serve para destroir e deixar vazio a sessão.

--- Principal.php
+++ Principal.php
@@ -1,6 +1,12 @@
 <?php
     include 'php/conexao.php';
     session_start();
-    echo 'Seja Bem vindo! : '.$_SESSION['nome'];
+    if(isset($_SESSION['nome'])){
+        echo 'Seja Bem vindo! : '.$_SESSION['nome'].'<br>';
+        echo "<a href='php/sair.php'>Sair</a>";
+    }else{
+        echo "Página Restrita, Por favor realizar Login: <a href='index.php'> Login </a>";
+    }
+    
     
 ?>
